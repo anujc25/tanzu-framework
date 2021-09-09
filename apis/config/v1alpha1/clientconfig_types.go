26c4ffaa9e3185fb2220f22a9a81e8bcac9cda6e
Anuj Chaudhari <anujc@vmware.com>
2021-09-09 15:22:29 -0700

    Add feature flag to use PluginAPI

--- apis/config/v1alpha1/clientconfig_types.go
+++ apis/config/v1alpha1/clientconfig_types.go
@@ -102,6 +102,9 @@ type CLIOptions struct {
 	DiscoverySources []PluginDiscovery `json:"discoverySources,omitempty" yaml:"discoverySources"`
 	// UnstableVersionSelector determined which version tags are allowed
 	UnstableVersionSelector VersionSelectorLevel `json:"unstableVersionSelector,omitempty" yaml:"unstableVersionSelector"`
+	// UsePluginAPI determines whether to use legacy way of discovering plugins or
+	// to use new Plugin API based plugin descovery
+	UsePluginAPI bool `json:"usePluginAPI,omitempty" yaml:"usePluginAPI"`
 }
 
 // PluginDiscovery contains a specific distribution mechanism. Only one of the
