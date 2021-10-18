43ddb0bf235082168d67b9bc1799138bf733a4fc
Anuj Chaudhari <anujc@vmware.com>
2021-10-18 16:40:27 -0700

    Use CLI feature flags instead of using direct config to gate this feature
    - This change will use CLI feature flag implementation intead of using
      config directly to gate contextAwarePluginDiscovery work
    
    Signed-off-by: Anuj Chaudhari <anujc@vmware.com>

--- pkg/v1/config/clientconfig.go
+++ pkg/v1/config/clientconfig.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/aunum/log"
 	"github.com/pkg/errors"
@@ -401,13 +402,29 @@ func EndpointFromServer(s *configv1alpha1.Server) (endpoint string, err error) {
 }
 
 // IsContextAwareDiscoveryEnabled returns true if context-aware discovery is enabled
+// User can set this CLI feature flag using `tanzu config set features.global.useContextAwareDiscovery true`
+// This determines whether to use legacy way of discovering plugins or
+// to use the new context-aware Plugin API based plugin discovery mechanism
 func IsContextAwareDiscoveryEnabled() bool {
+	contextAwareDiscoveryEnabled := false // Default value is set to false
+
 	cfg, err := GetClientConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
-	if cfg == nil || cfg.ClientOptions == nil || cfg.ClientOptions.CLI == nil {
-		return false
+	if cfg == nil || cfg.ClientOptions == nil {
+		return contextAwareDiscoveryEnabled
 	}
-	return cfg.ClientOptions.CLI.UseContextAwareDiscovery
+
+	fm, exists := cfg.ClientOptions.Features["global"]
+	if !exists {
+		return contextAwareDiscoveryEnabled
+	}
+
+	contextAwareDiscoveryEnabledString, exists := fm["useContextAwareDiscovery"]
+	if !exists {
+		return contextAwareDiscoveryEnabled
+	}
+
+	return strings.EqualFold(contextAwareDiscoveryEnabledString, "true")
 }
