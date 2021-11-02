e9bfb4c268a34883251234912e5f6842251c0266
Harish Yayi <yharish991@gmail.com>
2021-11-02 17:16:49 -0400

    add CRS for featuregates package
    Signed-off-by: Harish Yayi <yharish991@gmail.com>

--- pkg/v1/tkg/client/upgrade_cluster.go
+++ pkg/v1/tkg/client/upgrade_cluster.go
@@ -348,7 +348,8 @@ func (c *TkgClient) upgradeAddonPreNodeUpgrade(regionalClusterClient clusterclie
 			"addons-management/tanzu-addons-manager",
 			"tkr/tkr-controller",
 			"addons-management/core-package-repo",
-			"packages/management-package-repo")
+			"packages/management-package-repo," +
+			"packages/management-package")
 	}
 	upgradeClusterMetadataOptions := &UpgradeAddonOptions{
 		AddonNames:        addonsToBeUpgraded,
